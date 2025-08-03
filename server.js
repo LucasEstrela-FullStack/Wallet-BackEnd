17b44d11bfa7360e3e5b9f9efe3ac80a223b0e8a
LucasEstrela-FullStack <l1abreuestrela@gmail.com>
2025-08-03 00:28:26 -0400

    Create Rota Transanctions

--- server.js
+++ server.js
@@ -6,6 +6,13 @@ dotenv.config()
 
 const app = express()
 
+// Middleware
+app.use(express.json());
+// app.use((req,res,next) => {
+//   console.log("Opa encontramos uma requisição, o metódo é", req.method)
+//   next()
+// })
+
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -26,9 +33,32 @@ async function initDB() {
   }
 }
 
-app.get("/", (req, res) => {
-  res.send("O Servidor está trabalhando");
-});
+app.get("/", (req,res) => {
+  res.send("Está trabalhando @#")
+})
+
+app.post("/api/transactions", async (req,res) => {
+  // title, amount, category, user_id
+  try {
+    const {title, amount, category, user_id} = req.body;
+
+    if(!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({message: "Todos os campos estão preenchidos"});
+    }
+
+    const  transactions = await sql`
+    INSERT INTO transactions (user_id,title,amount,category)
+    VALUES(${user_id},${title},${amount},${category})
+    RETURNING * 
+    `
+    console.log(transactions)
+    res.status(201).json(transactions[0])
+
+  } catch (error) {
+    console.log("Erro ao criar a transação", error)
+    res.status(500).json({message:"Internal Server error"})
+  }
+})
 
 initDB().then(() => {
   app.listen(PORT,() =>{
